f7a416f22f10766b19d92bd68ca2d7607f358f04
danielmegale <danim1@live.it>
2023-12-12 14:37:43 +0100

    first commit

---
+++ src/main/java/org/java/spring/SpringLaMiaPizzeriaCrudApplication.java
@@ -0,0 +1,53 @@
+package org.java.spring;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.java.spring.db.pojo.Coupon;
+import org.java.spring.db.pojo.Ingredient;
+import org.java.spring.db.pojo.Pizza;
+import org.java.spring.db.service.CouponService;
+import org.java.spring.db.service.IngredientService;
+import org.java.spring.db.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PizzaService pizzaService;
+	@Autowired
+	private CouponService couponService;
+	@Autowired
+	private IngredientService ingredientService;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+	}
+	@Override
+	public void run(String...args) throws Exception{
+		Ingredient ing1=new Ingredient("nome1");
+		Ingredient ing2=new Ingredient("nome2");
+		Ingredient ing3=new Ingredient("nome3");
+		Ingredient ing4=new Ingredient("nome4");
+		
+		ingredientService.save(ing1);
+		ingredientService.save(ing2);
+		ingredientService.save(ing3);
+		ingredientService.save(ing4);
+		
+		pizzaService.save(new Pizza("Margherita", "Pizza con modoro e mozzarella", 4.99, null,ing1,ing2));
+		pizzaService.save(new Pizza("Patatosa", "Pizza con modoro, mozzarella e patatine fritte", 6.99, null,ing1,ing2,ing4));
+		pizzaService.save(new Pizza("4 Formaggi", "Pizza con mozzarella, Certosino, Gorgonzola D.O.P. Gim e parmigiano grattugiato,", 6.99, null,ing2,ing4));
+		pizzaService.save(new Pizza("Prosciutto cotto", "Pizza con modoro, mozzarella e prosciutto cotto", 6.99, null,ing3,ing1));
+		
+		List <Pizza> pizzas =pizzaService.findAll();
+		couponService.save(new Coupon("coupon 1",LocalDate.now(),LocalDate.now().plusDays(5),pizzas.get(0)));
+		couponService.save(new Coupon("coupon 2",LocalDate.now(),LocalDate.now().plusDays(10),pizzas.get(1)));
+		couponService.save(new Coupon("coupon 3",LocalDate.now(),LocalDate.now().plusDays(3),pizzas.get(2)));
+		couponService.save(new Coupon("coupon 4",LocalDate.now(),LocalDate.now().plusDays(8),pizzas.get(3)));
+	
+		}
+}
